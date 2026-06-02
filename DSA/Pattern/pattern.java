f354b13211be5487116e36a39cd0f92bf2a12477
abhi27-shek <ak9123411@gmail.com>
2026-06-02 20:44:01 +0530

    floyds

--- DSA/Pattern/pattern.java
+++ DSA/Pattern/pattern.java
@@ -34,11 +34,23 @@ public class pattern {
         }
     }
 
-    public static
+    public static void floyds_triangle(int n){
+        //outer
+        int counter = 1;
+        for(int i=1; i<=n; i++){
+            //inner - how many times will counter be printed
+            for(int j=1; j<=i; j++){
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String args[]){
         //hollow_rectangle(4,5);
         //inverted_rotated_pyramid(4);
+        //floyds_triangle(n);
     }
 
 }
