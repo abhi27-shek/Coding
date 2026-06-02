9f03eb3cda7b56c17238b46943897c55377b5ca5
abhi27-shek <ak9123411@gmail.com>
2026-06-02 20:32:09 +0530

    delete and update

---
+++ DSA/Pattern/pattern.java
@@ -0,0 +1,44 @@
+public class pattern {
+
+    public static void hollow_rectangle(int totRows, int totCols){
+        //outer loop
+        for(int i=1; i<=totRows; i++){
+            //inner - columns
+            for(int j=1; j<=totCols; j++){
+                //cell - (i,j)
+                if(i== 1 || i == totRows || j==1 || j == totCols){
+                    //boundary cells
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void inverted_rotated_pyramid(int n){
+        //outer
+        for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            //stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static
+
+    public static void main(String args[]){
+        //hollow_rectangle(4,5);
+        //inverted_rotated_pyramid(4);
+    }
+
+}
