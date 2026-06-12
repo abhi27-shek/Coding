6a8793d44b6ba8f0029ca760a370168aec1d72a2
Abhishek kumar <ak9123411@gmail.com>
2026-06-12 21:24:19 +0530

    backproblems

--- DSA/BackTracking/BackTracking.java
+++ DSA/BackTracking/BackTracking.java
@@ -3,7 +3,7 @@ package DSA.BackTracking;
 public class BackTracking {
 
 //------------------------------------------------------------------
-//changeArr
+    //changeArr
     public static void changeArr(int arr[], int i, int val){
         //base case
         if(i == arr.length){
@@ -14,6 +14,13 @@ public class BackTracking {
         arr[i]=val;
         changeArr(arr, i+1, val+1);
         arr[i]= arr[i]-2;
+    }
+    //print arr
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 //------------------------------------------------------------------
 //find subset of string
@@ -34,19 +41,60 @@ public class BackTracking {
         findSubsets(str, ans, i+1);
     }
 //------------------------------------------------------------------
-//print arr
-    public static void printArr(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+    public static void findPermutation(String str, String ans){
+        //base
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
         }
-        System.out.println();
+        
+        //recursion
+        for(int i=0; i<str.length(); i++){
+            char curr = str.charAt(i);
+            //"abcde"=> "ab"+"de"="abde"
+            String NewStr = str.substring(0,i)+ str.substring(i+1);
+            findPermutation(NewStr, ans+curr);
+        }
     }
+//------------------------------------------------------------------
+    //nQueen problem
+    public static void nQueens(char board[][], int row){
+        //base
+        if(row == board.length){
+            printBoard(board);
+            return;
+        }
+        //column loop
+        for(int j=0; j<board.length; j++){
+            board[row][j] = 'Q';
+            nQueens(board, row+1);
+            board[row][j] = '.';
+        }
+    }
+
+    public static void printBoard(char board[][]){
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+        }
+    }
+//------------------------------------------------------------------
 
     public static void main(String[] args) {
         // int arr[] = new int[5];
         // changeArr(arr, 0, 1);
         // printArr(arr);
-        String str = "abc";
-        findSubsets(str, "", 0);
+        // String str = "abc";
+        // findSubsets(str, "", 0);
+        int n = 2;
+        char board[][] = new char[n][n];
+        //initialize
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j] = '.';
+            }
+        }
+        nQueens(board, 0);
     }
 }
