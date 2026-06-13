936c32abcd51e1980708dac2d2735fe344142626
Abhishek kumar <ak9123411@gmail.com>
2026-06-14 02:40:29 +0530

    addmiddle

--- DSA/LinkedList/LinkedList.java
+++ DSA/LinkedList/LinkedList.java
@@ -1,6 +1,7 @@
 package DSA.LinkedList;
 
-public class LinkedList {
+    //Create ll
+    public class LinkedList {
     public static class Node{
         int data;
         Node next;
@@ -13,7 +14,8 @@ public class LinkedList {
     public static Node head;
     public static Node tail;
 
-
+//------------------------------------------------------------------
+    //Add first in ll
     public void addFirst(int data){
         //Step1 = create new node
         Node newNode = new Node(data);
@@ -27,7 +29,8 @@ public class LinkedList {
         //Step3 - head = newNode
         head = newNode;
     }
-
+//------------------------------------------------------------------
+    //Add last in ll
     public void addLast(int data){
         Node newNode = new Node(data);
         if(head == null){
@@ -37,7 +40,8 @@ public class LinkedList {
         tail.next = newNode;
         tail = newNode;
     }
-
+//------------------------------------------------------------------
+    //Print ll
     public void print(){
         if(head == null){
             System.out.println("LL is empty");
@@ -49,17 +53,36 @@ public class LinkedList {
         }
         System.out.println("null");
     }
+//------------------------------------------------------------------
+    //Add node in middle
+    public void add(int idx, int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx-1){
+            temp = temp.next;
+            i++;
+        }
+
+        //i = idx-1; temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.print();
         ll.addFirst(1);
-        ll.print();
         ll.addFirst(2);
-        ll.print();
         ll.addLast(3);
-        ll.print();
         ll.addLast(4);
+
+        ll.add(2, 5);
         ll.print();
     }
 }
