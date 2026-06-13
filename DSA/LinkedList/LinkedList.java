5e8b49d42addea6964bef62cf80d40fe3bbf315d
Abhishek kumar <ak9123411@gmail.com>
2026-06-14 05:20:20 +0530

    removenth

--- DSA/LinkedList/LinkedList.java
+++ DSA/LinkedList/LinkedList.java
@@ -13,12 +13,14 @@ package DSA.LinkedList;
     }
     public static Node head;
     public static Node tail;
+    public static int size;
 
 //------------------------------------------------------------------
     //Add first in ll
     public void addFirst(int data){
         //Step1 = create new node
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -33,12 +35,34 @@ package DSA.LinkedList;
     //Add last in ll
     public void addLast(int data){
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
         }
         tail.next = newNode;
         tail = newNode;
+    }
+//------------------------------------------------------------------
+    //Add node in middle
+    public void add(int idx, int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx-1){
+            temp = temp.next;
+            i++;
+        }
+
+        //i = idx-1; temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
 //------------------------------------------------------------------
     //Print ll
@@ -54,24 +78,124 @@ package DSA.LinkedList;
         System.out.println("null");
     }
 //------------------------------------------------------------------
-    //Add node in middle
-    public void add(int idx, int data){
-        if(idx == 0){
-            addFirst(data);
-            return;
+    //remove first node
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("ll is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
         }
-        Node newNode = new Node(data);
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+//------------------------------------------------------------------
+    //remove last node
+    public int removeLast(){
+        if(size == 0){
+            System.out.print("ll is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        //prev : i = size-2;
+        Node prev = head;
+        for(int i=0; i<size-2; i++){
+            prev = prev.next;
+        }
+
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+//------------------------------------------------------------------
+    //Iterative search
+    public int itrSearch(int key){
         Node temp = head;
         int i = 0;
 
-        while(i < idx-1){
+        while(temp != null){
+            if(temp.data == key){ //key found
+                return i;
+            }
             temp = temp.next;
             i++;
         }
 
-        //i = idx-1; temp -> prev
-        newNode.next = temp.next;
-        temp.next = newNode;
+        //key not found
+        return -1;
+    }
+//------------------------------------------------------------------
+    //Recursive search
+    public int helper(Node head, int key){
+        if(head == null){
+            return -1;
+        }
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+        return idx+1;
+    }
+
+    public int recSearch(int key){
+        return helper(head, key);
+    }
+//------------------------------------------------------------------
+    //Reverse linked list
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+//------------------------------------------------------------------
+    //Find & remove Nth Node
+    public void deleteNthfromEnd(int n){
+        //calculate size
+        int sz = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+
+        if(n == sz){
+            head = head.next; //remove first
+            return;
+        }
+
+        //sz-n
+        int i =1;
+        int iTofind = sz-n;
+        Node prev = head;
+        while(i < iTofind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
     }
 
 
@@ -81,8 +205,17 @@ package DSA.LinkedList;
         ll.addFirst(2);
         ll.addLast(3);
         ll.addLast(4);
-
         ll.add(2, 5);
-        ll.print();
+       // System.out.println(ll.size);
+       //ll.removeFirst();
+       //ll.removeLast();
+       //ll.reverse();
+
+    //    System.out.println(ll.recSearch(3));
+    //    System.out.println(ll.recSearch(10));
+
+       ll.print();
+       ll.deleteNthfromEnd(3);
+       ll.print();
     }
 }
