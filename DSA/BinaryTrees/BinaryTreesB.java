38db87238c86a9d95fa77103647c5ac749b17d32
Abhishek kumar <ak9123411@gmail.com>
2026-06-25 21:56:18 +0530

    level order

--- DSA/BinaryTrees/BinaryTreesB.java
+++ DSA/BinaryTrees/BinaryTreesB.java
@@ -1,4 +1,5 @@
 package DSA.BinaryTrees;
+import java.util.*;
 
 public class BinaryTreesB {
     static class Node {
@@ -14,9 +15,9 @@ public class BinaryTreesB {
     }
     static class BinaryTree{
         static int idx = -1;
-        public static Node buildTree(int nodes[]){
+        public static Node buildTree(int nodes[]) {
             idx++;
-            if(nodes[idx] == -1){
+            if (idx >= nodes.length || nodes[idx] == -1) {
                 return null;
             }
 
@@ -44,7 +45,9 @@ public class BinaryTreesB {
             System.out.print(root.data+" ");
             inorder(root.right);
         }
-    }
+
+
+    
 
         public static void postorder(Node root){
             if(root == null){
@@ -54,13 +57,45 @@ public class BinaryTreesB {
             postorder(root.right);
             System.out.print(root.data+" ");
         }
+//****************************************************************************************
+        //Level Order Traversal        
+        public static void levelOrder(Node root){
+            if(root == null){
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
 
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode == null) {
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left != null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right != null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
+//****************************************************************************************
+    }
     public static void main(String[] args) {
-        int nodes[] = {1, 2, 4, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
 
-        tree.preorder(root);
+       //tree.preorder(root);
+       tree.levelOrder(root);
     }
 }
